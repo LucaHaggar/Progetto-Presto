980e6cb72465968c86b9d8b36f22cd94493ab83c
lucahaggar <luca.haggar1@gmail.com>
2023-05-14 10:17:15 +0200

    progetto presto terminato

---
+++ resources/views/noi/chiSiamo.blade.php
@@ -0,0 +1,257 @@
+<x-layout>
+    <x-header title="Chi Siamo"/>
+
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">DEV</span>
+            <img class="round img-chiSiamo" src="https://avatars.githubusercontent.com/u/125573764?v=4" alt="user" />
+            <h3 class="text-dark">Luca Haggar</h3>
+            <h6 class="text-dark">Roma, IT</h6>
+            <p>Full Stack web Developer</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/luca-haggar-b431a2206/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">DEV</span>
+            <img class="round img-chiSiamo" src="https://avatars.githubusercontent.com/u/125560216?v=4" alt="user" />
+            <h3 class="text-dark">Davide Benedetti</h3>
+            <h6 class="text-dark">Roma, IT</h6>
+            <p>Full Stack web Developer</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/davide-benedetti-web-developer/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">DEV</span>
+            <img class="round img-chiSiamo" src="https://avatars.githubusercontent.com/u/125572147?v=4" alt="user" />
+            <h3 class="text-dark">Davide Lai</h3>
+            <h6 class="text-dark">Cagliari, IT</h6>
+            <p>Full Stack web Developer</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/davide-lai-b3b6261b1/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">DEV</span>
+            <img class="round img-chiSiamo" src="https://media.licdn.com/dms/image/D4D03AQGOHM-KCOBtIA/profile-displayphoto-shrink_200_200/0/1679914879918?e=1689206400&v=beta&t=GjpX5nNhuyGaDfw4Q6ZuMqDK5UpEIoTy74wF1TINwVw" alt="user" />
+            <h3 class="text-dark">Fatima Chaouki</h3>
+            <h6 class="text-dark">Monza, IT</h6>
+            <p>Full Stack web Developer</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/fatima-chaouki-6029151bb/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">DEV</span>
+            <img class="round img-chiSiamo" src="https://media.licdn.com/dms/image/D4E03AQFzanhQ48bXhw/profile-displayphoto-shrink_800_800/0/1681371474794?e=1689206400&v=beta&t=7bOPDGvNgBZNFeIZtl4lxnbTPejZ7Sn4Rvhgonon6zY" alt="user" />
+            <h3 class="text-dark">Javier De La Cuesta</h3>
+            <h6 class="text-dark">Torino, IT</h6>
+            <p>Full Stack web Developer</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/javier-de-la-cuesta-jaraquemada-639054169/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="card-container mb-5">
+            <span class="pro">SCRUM</span>
+            <img class="round img-chiSiamo" src="https://media.licdn.com/dms/image/C4E03AQHDfa-mENbR_A/profile-displayphoto-shrink_200_200/0/1643640992935?e=1689206400&v=beta&t=b2CQaX8txiI2to1Hk2Cin5xDd5GCZNPwihGbGsA-xg4" alt="user" />
+            <h3 class="text-dark">Roerto Sasso</h3>
+            <h6 class="text-dark">Bari, IT</h6>
+            <p>SCRUM MASTER</p>
+            <div class="buttons">
+              <a href="https://www.linkedin.com/in/roberto-sasso-fullstack/">  <button class="primary ghost">Linkedin</button></a>
+            </div>
+            <div class="skills">
+              <h6>Skills</h6>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+              </ul>
+              <ul>
+                <li>Back-end Development</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Metodologie Agili</li>
+                <li>Docente Back-end</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    {{-- Start Timeline --}}
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="animate__animated animate__bounceInLeft h2-timeline">
+            TIMELINE AULAB
+          </h2>
+      
+          <div class="timeline">      
+            <div
+              class="timeline__event animated fadeInUp delay-2s timeline__event--type2"
+            >
+              <div class="timeline__event__icon">
+      
+              </div>
+              <div class="timeline__event__date">6 Febbraio 2023</div>
+              <div class="timeline__event__content">
+                <div class="timeline__event__title">Inizio Hackademy 69</div>
+                <div class="timeline__event__description">
+                  <p class="dark-timeline">Inizio Bootcamp Web Developer Full Immersion</p>
+                </div>
+              </div>
+            </div>
+      
+      
+      
+            <div
+              class="timeline__event animated fadeInUp delay-1s timeline__event--type3"
+            >
+              <div class="timeline__event__icon">
+      
+              </div>
+              <div class="timeline__event__date">6 Febbraio <br> 14 Marzo</div>
+              <div class="timeline__event__content">
+                <div class="timeline__event__title">Front-end Skill</div>
+                <div class="timeline__event__description">
+                  <p class="dark-timeline">
+                    Acquisite competenze in HTML | CSS | JAVASCRIP | Manipolazione del
+                    DOM
+                  </p>
+                </div>
+              </div>
+            </div>
+      
+      
+      
+            <div class="timeline__event animated fadeInUp timeline__event--type1">
+              <div class="timeline__event__icon">
+        
+              </div>
+              <div class="timeline__event__date">15 Marzo <br> 18 Aprile</div>
+              <div class="timeline__event__content">
+                <div class="timeline__event__title">Back-end Skill</div>
+                <div class="timeline__event__description">
+                  <p class="dark-timeline">PHP | Laravel | MySql</p>
+                </div>
+              </div>
+            </div>
+      
+      
+            <div
+              class="timeline__event animated fadeInUp delay-3s timeline__event--type1"
+            >
+              <div class="timeline__event__icon">
+      
+              </div>
+              <div class="timeline__event__date">Maggio 2023</div>
+              <div class="timeline__event__content">
+                <div class="timeline__event__title">Presto.IT</div>
+                <div class="timeline__event__description">
+                  <p class="dark-timeline">
+                    Sito e-commerce Sviluppato in Team con metodologia Agile SCRUM
+                  </p>
+                </div>
+              </div>
+            </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+  </x-layout>
+  
